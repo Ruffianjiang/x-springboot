aa1f9f628748c769446aa2db977159dd28dea449
ruffianjiang <jiangyj0516@outlook.com>
2021-01-27 13:27:57 +0800

    目录迁移

---
+++ service/src/test/java/com/suke/czx/XSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suke.czx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
