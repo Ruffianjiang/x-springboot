b902eee78d74a12413dbf4a529c24299c6612e99
jiang <jiangyongji@pintechs.com>
2025-05-06 10:14:17 +0800

    fix bugs

--- service/src/main/java/com/suke/czx/Swagger2.java
+++ service/src/main/java/com/suke/czx/Swagger2.java
@@ -9,13 +9,13 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 /**
  * Created by czx on 2017/10/31.
  */
 @Configuration
-@EnableSwagger2
+@EnableSwagger2WebMvc
 public class Swagger2 {
 
     /**
